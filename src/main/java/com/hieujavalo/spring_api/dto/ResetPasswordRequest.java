86493a4feab36193a3b294150e55710cf61414ba
Hieu Ng <114124917+HieuJamesNg204@users.noreply.github.com>
2025-12-04 09:34:35 +0700

    Add password length validation to ResetPasswordRequest

--- src/main/java/com/hieujavalo/spring_api/dto/ResetPasswordRequest.java
+++ src/main/java/com/hieujavalo/spring_api/dto/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.hieujavalo.spring_api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ public class ResetPasswordRequest {
     private String code;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
